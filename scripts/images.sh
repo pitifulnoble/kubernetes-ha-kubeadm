fa9c46e6b3226a4bff2bf22e7510f4ba806bf54f
saxingz <saxlh@foxmail.com>
2020-06-22 19:09:45 +0800

    feat: kubernetes

--- scripts/images.sh
+++ scripts/images.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-docker pull 172.16.249.159:8082/calico/node:v3.14.1
-docker tag 172.16.249.159:8082/calico/node:v3.14.1 calico/node:v3.14.1
-docker rmi 172.16.249.159:8082/calico/node:v3.14.1
+docker pull calico/node:v3.14.1
 
-docker pull 172.16.249.159:8082/calico/typha:v3.14.1
-docker tag 172.16.249.159:8082/calico/typha:v3.14.1 calico/typha:v3.14.1
-docker rmi 172.16.249.159:8082/calico/typha:v3.14.1
+docker pull calico/typha:v3.14.1
 
-docker pull 172.16.249.159:8082/calico/cni:v3.14.1
-docker tag 172.16.249.159:8082/calico/cni:v3.14.1 calico/cni:v3.14.1
-docker rmi 172.16.249.159:8082/calico/cni:v3.14.1
+docker pull calico/cni:v3.14.1
 
-docker pull 172.16.249.159:8082/calico/kube-controllers:v3.14.1
-docker tag 172.16.249.159:8082/calico/kube-controllers:v3.14.1 calico/kube-controllers:v3.14.1
-docker rmi 172.16.249.159:8082/calico/kube-controllers:v3.14.1
+docker pull calico/kube-controllers:v3.14.1
 
-docker pull 172.16.249.159:8082/calico/pod2daemon-flexvol:v3.14.1
-docker tag 172.16.249.159:8082/calico/pod2daemon-flexvol:v3.14.1 calico/pod2daemon-flexvol:v3.14.1
-docker rmi 172.16.249.159:8082/calico/pod2daemon-flexvol:v3.14.1
+docker pull calico/pod2daemon-flexvol:v3.14.1
+
